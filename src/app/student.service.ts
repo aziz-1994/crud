8f08299dc4a8c6e0b2fa393917c564c11c268571
aziz-1994 <57746751+aziz-1994@users.noreply.github.com>
2020-01-01 16:11:08 +0530

    Update student.service.ts

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -8,20 +8,20 @@ export class StudentService {
 
   constructor(private http:HttpClient) { }
   getStudent(){
-    return this.http.get<Student[]>('http://localhost/angular_crud/list.php');
+    return this.http.get<Student[]>('https://abdulaziz94.000webhostapp.com/angular_crud/angular_crud/angular_crud/list.php');
   }
   deleteStudent(id:number){
-    return this.http.delete<Student[]>('http://localhost/angular_crud/delete.php?id=' + id);
+    return this.http.delete<Student[]>('https://abdulaziz94.000webhostapp.com/angular_crud/angular_crud/angular_crud/delete.php?id=' + id);
   }
   createStudent(student: Student){
-    return this.http.post('http://localhost/angular_crud/insert.php', student);
+    return this.http.post('https://abdulaziz94.000webhostapp.com/angular_crud/angular_crud/angular_crud/insert.php', student);
   }
   gteById(id:number){
-    return this.http.get<Student[]>('http://localhost/angular_crud/getById.php?id=' + id);
+    return this.http.get<Student[]>('https://abdulaziz94.000webhostapp.com/angular_crud/angular_crud/angular_crud/getById.php?id=' + id);
   }
 
   updateStudent(student: Student){
 
-  return this.http.put('http://localhost/angular_crud/update.php' + '?id=' + student.sId, student);
+  return this.http.put('https://abdulaziz94.000webhostapp.com/angular_crud/angular_crud/angular_crud/update.php' + '?id=' + student.sId, student);
   }
 }
